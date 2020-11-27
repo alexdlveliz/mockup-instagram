754b00a32febabdceca065a3c96ee229e6a6ffcc
alexdlveliz <alexdlveliz@protonmail.com>
2020-11-27 17:20:02 -0600

    Header terminado

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,32 @@ function Header() {
           <img src={Logo} alt="Logo" />
           <h1>Instagram</h1>
         </div>
+
+        <div id="searchbar">
+          <div id="searchbar-outline">
+            <i class="fas fa-search"></i>
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+
+        <div id="actions">
+          <button id="new-post">
+            <i class="fas fa-plus-circle"></i> Add a new post
+          </button>
+
+          <button id="profile">
+            Profile
+            <img src="https://picsum.photos/200" alt="profile" />
+          </button>
+
+          <button id="notifications">
+            <i class="far fa-bell"></i>
+          </button>
+
+          <button id="messages">
+            <i class="far fa-envelope"></i>
+          </button>
+        </div>
       </header>
     </React.Fragment>
   );
